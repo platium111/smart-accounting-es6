16188938f7ff542fa0c2e6ab809201a136928c20
Clark Trinh <clark.trinh@nab.com.au>
2019-07-12 07:16:35 +1000

    Fix corc from server

--- dist/src/app.js
+++ dist/src/app.js
@@ -8,16 +8,21 @@ var _expressGraphql = require("express-graphql");
 
 var _expressGraphql2 = _interopRequireDefault(_expressGraphql);
 
+var _mongoose = require("mongoose");
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _bodyParser = require("body-parser");
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 var _schema = require("./schema/schema");
 
 var _schema2 = _interopRequireDefault(_schema);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var mongoose = require("mongoose");
-
-var bodyParser = require("body-parser");
-
+var cors = require("cors");
 
 var app = (0, _express2.default)();
 
@@ -26,15 +31,16 @@ if (!MONGO_URI) {
   throw new Error("you don't provide MongoDB URI");
 }
 
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
-mongoose.connection.once("open", function () {
+_mongoose2.default.Promise = global.Promise;
+_mongoose2.default.connect(MONGO_URI, { useNewUrlParser: true });
+_mongoose2.default.connection.once("open", function () {
   return console.log("Connected to MongoLab instance.");
 }).on("error", function (error) {
   return console.log("Error connecting to MongoLab:", error);
 });
 
-app.use(bodyParser.json());
+app.use(cors());
+app.use(_bodyParser2.default.json());
 
 app.use("/graphql", (0, _expressGraphql2.default)({
   schema: _schema2.default,
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import expressGraphQL from "express-graphql";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import schema from "./schema/schema";
+const cors = require("cors");
 
 const app = express();
 
@@ -18,6 +19,7 @@ mongoose.connection
   .once("open", () => console.log("Connected to MongoLab instance."))
   .on("error", error => console.log("Error connecting to MongoLab:", error));
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(
